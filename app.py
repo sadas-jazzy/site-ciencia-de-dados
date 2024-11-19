5b7390e7db3569712aa883494f6c3f3bbef99e26
sadas-jazzy <raul.martinst@gmail.com>
2024-11-19 09:04:20 -0300

    .

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = flask(__name__, template_folder ="sitenciencia/templates", static_folder =
 def index():
     #return "<h1>Bom dia, professor.</h1>"
     return render_template("index.html")
+
+@app.route("/analise ")
+@app.route("/analise/<usuario>")
+def analise(usuario):
+    return render_template("analise.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
